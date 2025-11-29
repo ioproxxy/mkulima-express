4f4b9e554d74645f8958b0f6c2a71ce70bc4a77a
ioproxxy <ioproxxy@gmail.com>
2025-11-29 21:46:15 +0300

    update

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './src/App.tsx';
+import App from './App.tsx';
 import { DataProvider } from './contexts/DataContext.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import 'react-toastify/dist/ReactToastify.css';
